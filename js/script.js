895b6755900ec14c8fe45923b99ba2f63c54f8d5
Arkadiusz Zygan <a.zygan@gmail.com>
2020-06-09 14:39:54 +0200

    Edit styles

--- js/script.js
+++ js/script.js
@@ -38,6 +38,15 @@ function reduceToDigit(n) {
 
 function getDay() {
   let day = dayInput.value;
+
+  if (day === '') {
+    dayInput.parentElement.querySelector('.info').innerHTML = 'Wypełnij pole';
+    return    
+  } else if (0 >= day || day > 31) {
+    dayInput.parentElement.querySelector('.info').innerHTML = 'Podaj poprawny dzień';
+    return
+  } else
+  dayInput.parentElement.querySelector('.info').innerHTML = '';
   if (day >= 10) day = reduceToDigit(day);
   if (day >= 10) day = reduceToDigit(day);
   return day
@@ -45,21 +54,43 @@ function getDay() {
 
 function getMonth() {
   let month = monthInput.value;
-  month = parseInt(month)
+  if (month === '') {
+    monthInput.parentElement.querySelector('.info').innerHTML = 'Wypełnij pole';
+    return month
+  }
+  monthInput.parentElement.querySelector('.info').innerHTML = '';
+  month = parseInt(month);
   return month
+}
+
+function getYear() {
+  let year = yearInput.value;
+  let currentYear = new Date().getFullYear();
+  if (year === '') {
+    yearInput.parentElement.querySelector('.info').innerHTML = 'Wypełnij pole';
+    return
+  } else if (year > currentYear) {
+    yearInput.parentElement.querySelector('.info').innerHTML = 'Podaj poprawny rok';
+    return
+  } else
+  yearInput.parentElement.querySelector('.info').innerHTML = '';
+  if (year >= 10) year = reduceToDigit(year);
+  if (year >= 10) year = reduceToDigit(year);
+  return year
 }
 
 function putDestiny() {
   let day = getDay();
   let month = getMonth();
-  let year = yearInput.value;
-  year = reduceToDigit(year);
-  year = reduceToDigit(year);
+  let year = getYear();
+
+  if (day === undefined || month === '' || year === undefined) {
+    return
+  } 
 
   let destiny = day + month + year;
   if (destiny >= 10) destiny = reduceToDigit(destiny);
   if (destiny >= 10) destiny = reduceToDigit(destiny);
-
   document.querySelector('#destiny #destinyOutput').innerHTML = destiny;
   document.querySelector('#destiny .descContent').innerHTML = destinyNumber[destiny].description;
   document.querySelector('.destinyNumber').innerHTML = destiny;
@@ -72,9 +103,12 @@ function putPersonalYear() {
   document.querySelector('#personal-year #yearOutput').innerHTML = currentYear;
   if (currentYear >= 10) currentYear = reduceToDigit(currentYear);
   if (currentYear >= 10) currentYear = reduceToDigit(currentYear);
-
+  
+  if (day === undefined || month === undefined || currentYear === undefined) {
+    return
+  }
+ 
   let personalYear = day + month + currentYear;
-  console.log(personalYear)
   if (personalYear >= 10) personalYear = reduceToDigit(personalYear);
 
   document.querySelector('#personal-year .descContent').innerHTML = personalYearDB[personalYear].description;
@@ -82,12 +116,17 @@ function putPersonalYear() {
 
 function putBirthday() {
   let birthday = dayInput.value;
+
+  if (birthday === '') {
+    return
+  }
   document.querySelector('#birthday #birthdayOutput').innerHTML = birthday;
   document.querySelector('#birthday .descContent').innerHTML = birthdayNumber[birthday].description;
 }
 
 
 checkBtn.addEventListener('click', () => {
+
   putDestiny();
   putPersonalYear();
   putBirthday();
